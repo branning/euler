f87ddbded63944d6babb5ffd75de73a3192d1e22
Philip <branning@gmail.com>
2014-03-06 10:33:05 -0800

    add memoization, print answer only

--- euler014.py
+++ euler014.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from euler007 import eratosthenes
+import time
 
 def collatz_sequence(n):
     sequence = [n]
@@ -9,15 +10,21 @@ def collatz_sequence(n):
         sequence.append(n)
     return sequence
 
-def collatz_steps(n):
+def collatz_steps(n, memos):
     steps = 1
     while n > 1:
         n = (n/2) if n % 2 == 0 else (3*n+1)
-        steps += 1
+        if n in memos:
+            steps += memos[n]
+            break
+        else:
+            steps += 1
     return steps
 
 
 if __name__=="__main__":
+    debugging = False
+    tic = time.time()
     #primes = [2*e for e in eratosthenes(5*10**5)]
     numbers = [i for i in xrange(10*10**5, 5*10**5, -1)]
 
@@ -26,9 +33,13 @@ if __name__=="__main__":
     maxsteps = 0
     maxn = None
     for n in xrange(-1, -1-len(numbers), -1):
-        steps[numbers[n]] = collatz_steps(numbers[n])
+        steps[numbers[n]] = collatz_steps(numbers[n], steps)
         if steps[numbers[n]] > maxsteps:
             maxsteps = steps[numbers[n]]
             maxn = numbers[n]
         #print "{}: {} --> {} steps".format(-1*n, numbers[n], steps[numbers[n]])
-    print "max steps: {} --> {}".format(maxn, steps[maxn])
+    if debugging:
+        print "max steps: {} --> {}".format(maxn, steps[maxn])
+        print "{:0} s".format(int(time.time() - tic))
+    else:
+        print maxn
