86fd092bcc80a136ae491ef8b1a420bdf2a964f3
Philip <branning@gmail.com>
2014-03-06 09:45:21 -0800

    only print output, actually call gridSearch

--- euler004.py
+++ euler004.py
@@ -14,12 +14,19 @@ def analytical_reverse(n):
     return reversed
 
 def gridSearch(x_end, y_end):
+    largest_palindrome = None
     found = False
     for x in xrange(999, x_end - 1, -1):
         for y in xrange(999, y_end - 1, -1):
             if isPalindrome(x*y):
                 found = True
-                print "{} * {} is a palindrome!".format(x, y)
+#               print "{} * {} is a palindrome!".format(x, y)
+                largest_palindrome = x*y
+                return largest_palindrome
                 break
         if found:
             break
+
+if __name__=="__main__":
+    # It's likely the highest palindromic product is near 999 * 999. Look there
+    print gridSearch(900,900)
