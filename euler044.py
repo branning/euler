986a2eba99392df46474af1fbdad1cafda35503d
Philip <branning@gmail.com>
2014-04-05 21:53:56 -0700

    add problem 44

---
+++ euler044.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import math
+
+def pentagon(n):
+  return n*(3*n-1)/2
+def inv_pentagon(n):
+  return (1+math.sqrt(24*n+1))/6
+
+if __name__=="__main__":
+  pents = set()
+  pent_pairs = []
+  i = 0
+  stop = False
+  while not stop:
+    i += 1
+    next = pentagon(i)
+    for p in pents:
+      diff = next-p
+      total = next+p
+      if inv_pentagon(diff) == math.floor(inv_pentagon(diff)) and \
+         inv_pentagon(total) == math.floor(inv_pentagon(total)):
+        pent_pairs.append((next,p))
+        stop = True
+    pents.add(next)
+    if i > 10000:
+      break
+  print min([a-b for a,b in pent_pairs])
