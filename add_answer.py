1a8178280106780e120daf996c4239c2061cfdfc
Philip <branning@gmail.com>
2015-03-16 16:49:09 -0700

    can add and remove answers to hashed answer file

--- add_answer.py
+++ add_answer.py
@@ -12,12 +12,21 @@ if __name__=="__main__":
     with open('answers.p', 'rb') as answers_pickle:
         answers = cPickle.load(answers_pickle)
     new_answers = {}
+    remove_answers = []
     for problem, answer in pairs(sys.argv[1:]):
-        print "{:4} {:12}".format(problem, answer)
+        problem = int(problem)
+        if answer == "remove":
+            remove_answers.append(problem)
+            print "{:4} remove".format(problem)
+            continue
+        print "{:4} {}".format(problem, answer),
+        print "*changed" if problem in answers else ''
         new_answers[problem] = bcrypt.hashpw(str(answer), bcrypt.gensalt())
-    print "Add these answers?"
     if raw_input('Add these answers? (y/n)').lower() == 'y':
-        updated_answers = answers.copy()
-        updated_answers.update(new_answers)
-    print updated_answers
+        answers.update(new_answers)
+        for answer in remove_answers:
+            del(answers[answer])
+    with open('answers.p', 'w') as answers_pickle:
+        cPickle.dump(answers, answers_pickle)
+        print "{} answers written to {}".format(len(answers), answers_pickle.name)
 
